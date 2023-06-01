0fde37ead795616a7fb0eb1d0b302b8fb11c8727
zidiks <zidiks228@gmail.com>
2023-06-01 11:51:14 +0300

    upd: switch to tailwindcss + features section

--- src/app/modules/pages/welcome/welcome.component.ts
+++ src/app/modules/pages/welcome/welcome.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface WelcomeFeature {
+  img: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -7,4 +12,26 @@ import { Component } from '@angular/core';
 })
 export class WelcomeComponent {
   public year = new Date().getFullYear();
+  public features: WelcomeFeature[] = [
+    {
+      img: 'assets/welcome/tests.svg',
+      name: 'Тесты',
+    },
+    {
+      img: 'assets/welcome/videos.svg',
+      name: 'Видео',
+    },
+    {
+      img: 'assets/welcome/seminars.svg',
+      name: 'Семинары',
+    },
+    {
+      img: 'assets/welcome/base.svg',
+      name: 'База знаний',
+    },
+    {
+      img: 'assets/welcome/presentations.svg',
+      name: 'Презентации',
+    },
+  ]
 }

775895e87acc4c10b34fbf361a5c8b5e8083999e
zidiks <zidiks228@gmail.com>
2024-03-15 09:23:37 +0300

    upd: implement login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE } from '@taiga-ui/i18n';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { of } from "rxjs";
 
 @NgModule({
     declarations: [
@@ -19,7 +21,10 @@ import { AppComponent } from './app.component';
         TuiDialogModule,
         TuiAlertModule
     ],
-    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
+    providers: [
+      { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+      { provide: TUI_LANGUAGE, useValue: of(TUI_RUSSIAN_LANGUAGE) },
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/pages/login/login.component.ts
+++ src/app/modules/pages/login/login.component.ts
@@ -1,15 +1,99 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from "@angular/common";
+import { AsyncPipe, CommonModule, NgOptimizedImage } from "@angular/common";
+import {
+  TUI_PASSWORD_TEXTS,
+  TUI_VALIDATION_ERRORS,
+  TuiFieldErrorPipeModule,
+  TuiInputPasswordModule,
+  TuiIslandModule,
+  TuiSelectModule
+} from "@taiga-ui/kit";
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import {
+  TuiButtonModule,
+  TuiDataListModule,
+  TuiErrorModule,
+  TuiLoaderModule,
+  TuiTextfieldControllerModule
+} from "@taiga-ui/core";
+import { TuiStringHandler, tuiPure, TuiContextWithImplicit, TuiLetModule } from "@taiga-ui/cdk";
+import { of } from "rxjs";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    TuiIslandModule,
+    ReactiveFormsModule,
+    TuiButtonModule,
+    TuiFieldErrorPipeModule,
+    AsyncPipe,
+    TuiErrorModule,
+    TuiFieldErrorPipeModule,
+    TuiTextfieldControllerModule,
+    TuiInputPasswordModule,
+    TuiLoaderModule,
+    TuiDataListModule,
+    TuiSelectModule,
+    TuiLetModule,
+    CommonModule,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  providers: [
+    {
+      provide: TUI_VALIDATION_ERRORS,
+      useValue: {
+        required: `Пустое поле!`,
+      },
+    },
+    {
+      provide: TUI_PASSWORD_TEXTS,
+      useValue: of([``]),
+    },
+  ],
 })
 export class LoginComponent {
+  public craList: { login: string, name: string }[] = [
+    {
+      login: 'cra16',
+      name: 'ЦРА №16 Борисовского района',
+    },
+    {
+      login: 'cra19',
+      name: 'ЦРА №19 Березинского района',
+    },
+    {
+      login: 'cra118',
+      name: 'ЦРА №118 Вилейского района',
+    },
+  ];
+  public formGroup: FormGroup = this.formBuilder.group( {
+    login : [ window.localStorage.getItem('mf_login') || null, Validators.required ],
+    password : [ null, Validators.required ],
+  } );
 
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  public onSubmit(): void {
+    this.formGroup.markAllAsTouched();
+
+    if (this.formGroup.invalid) {
+      return;
+    }
+  }
+
+  @tuiPure
+  public stringify(
+    items: { login: string, name: string }[],
+  ): TuiStringHandler<TuiContextWithImplicit<string>> {
+    const map = new Map(items.map(({login, name}) => [login, name] as [string, string]));
+
+    return ({$implicit}: TuiContextWithImplicit<string>) => map.get($implicit) || ``;
+  }
+
+  public get f(): { [key: string]: AbstractControl; } { return this.formGroup.controls; }
 }
